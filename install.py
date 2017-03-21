488f266b6b3aa03b158e74f69f72e36e673f7bef
Li Yao <yao@bogon.dhcp>
2017-03-22 00:18:24 +0800

    No message

--- install.py
+++ install.py
@@ -4,7 +4,6 @@ import sys
 from multiprocessing import cpu_count
 from getpass import getpass
 import ConfigParser
-import psutil
 byte_to_gigabyte = 1073741824
 
 def set_config(section, key, value):
@@ -50,6 +49,18 @@ def setup():
             print 'The path you input doesn\'t exist! Please reassign it.', e
             workspace_path = raw_input('Path of workspace: ')
 
+    print '===================================================='
+    print 'Installing dependent python packages, please wait...'
+    print '===================================================='
+
+    pip_import_path = os.path.split(os.path.realpath(__file__))[0] + '/deploy/prerequisites.txt'
+    if os.system('pip install -r %s' % pip_import_path):
+        print '====================================================='
+        print '|Fetal error occured when installing python packages|'
+        print '|Installation will be terminated now                |'
+        print '====================================================='
+        sys.exit(1)
+
     log_path = os.path.join(workspace_path, 'logs')
     output_path = os.path.join(workspace_path, 'outputs')
     train_path = os.path.join(workspace_path, 'training')
@@ -85,12 +96,18 @@ def setup():
     set_config('program', 'secret_key', secret_key)
     setting_file = setting_file.replace('{SECRET_KEY}', secret_key)
 
+    print ''
+    print '==================='
+    print 'Basic configuration'
+    print '==================='
+    print ''
+
     cpu_cores = str(cpu_count())
     user_cpu_cores = raw_input('CPU cores (By default: %s): ' % cpu_cores)
     if user_cpu_cores:
         cpu_cores = user_cpu_cores
     set_config('env', 'cpu', cpu_cores)
-
+    import psutil
     memory_gbs = round(psutil.virtual_memory().total / byte_to_gigabyte)
     user_memory = raw_input('Memory(Gb, by default: %s Gb): ' % memory_gbs)
     if user_memory:
@@ -103,9 +120,9 @@ def setup():
         disk_size = user_disk_size
     set_config('env', 'disk_quota', disk_size)
 
-    print '====================================================================================='
+    print '======================================='
     print 'Do you want to use BioQueue with MySQL? '
-    print '====================================================================================='
+    print '======================================='
     mysql_fb = raw_input('y/n (By default: y)')
 
     if mysql_fb == 'n':
@@ -126,9 +143,9 @@ def setup():
             db_port = '3306'
         database_configure['port'] = db_port
 
-        print '===================================================='
+        print '===================================='
         print 'Configuring database, please wait...'
-        print '===================================================='
+        print '===================================='
 
         set_config('db', 'host', database_configure['host'])
         set_config('db', 'user', database_configure['user'])
@@ -161,36 +178,25 @@ def setup():
     apache_handler.close()
     apache_handler_new.close()
 
-    print '===================================================='
-    print 'Installing dependent python packages, please wait...'
-    print '===================================================='
-
-    pip_import_path = os.path.split(os.path.realpath(__file__))[0] + '/deploy/prerequisites.txt'
-    if os.system('pip install -r %s' % pip_import_path):
-        print '====================================================='
-        print '|Fetal error occured when installing python packages|'
-        print '|Installation will be terminated now                |'
-        print '====================================================='
-
     django_manage_path = os.path.split(os.path.realpath(__file__))[0] + '/manage.py'
 
     init_data_path = os.path.split(os.path.realpath(__file__))[0] + '/init_resource.json'
 
-    print '===================================================='
+    print '==============================='
     print 'Creating tables, please wait...'
-    print '===================================================='
+    print '==============================='
 
     os.system('python %s migrate' % django_manage_path)
 
-    print '===================================================='
+    print '============================'
     print 'Loading data, please wait...'
-    print '===================================================='
+    print '============================'
 
     os.system('python %s loaddata %s' % (django_manage_path, init_data_path))
 
-    print '===================================================='
+    print '====================================='
     print 'Now we\'ll create a superuser account'
-    print '===================================================='
+    print '====================================='
 
     os.system('python %s createsuperuser' % django_manage_path)
 
